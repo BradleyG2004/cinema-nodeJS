2e64e9295857fc1247994c175d05b3c24ac44de6
Thamila92 <achatthamila@gmail.com>
2024-05-11 16:56:21 +0200

    add docker file

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "mysql-db",  
     port: 3306,
     username: "your_username",
     password: "your_password",
